579f088ef27f80284079cedfc7a5f9b994f89c8f
Sushavon Banerjee <sushavonb372@IN.nam.ad.pwcinternal.com>
2020-02-11 12:22:43 +0530

    Add project files.

---
+++ Product.Api/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Product.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return GetDummyProducts();
+        }
+
+        public IEnumerable<Product> GetDummyProducts()
+        {
+            var products = new List<Product>();
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Paracetamol",
+                Price = 1.00
+            };
+
+            products.Add(product);
+            return products;
+        }
+    }
+}
